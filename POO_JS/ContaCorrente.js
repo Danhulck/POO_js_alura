1b9f80d19130da8251c50f6878d7aa49bab974de
Danhulck <familiaevd@yahoo.com.br>
2025-08-14 11:20:25 -0300

    próximo do fim

--- POO_JS/ContaCorrente.js
+++ POO_JS/ContaCorrente.js
@@ -1,7 +1,30 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
-    agencia;
-    // #saldo = 0 Ver sobre a sintaxe privada: https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
+    agencia;    
+    _cliente;
+    _saldo = 0; // #saldo = 0 Ver sobre a sintaxe privada: https://github.com/tc39/proposal-class-fields#private-fields
+
+
+    
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+    
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo
+    }
+    
+    constructor(cliente, agencia){
+        theis.agencia = agencia;
+        this.cliente = cliente;
+    }
 
     sacar(valor) {
         if (this._saldo >= valor) {
@@ -15,5 +38,10 @@ export class ContaCorrente {
             return;
         }
         this._saldo += valor;
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
